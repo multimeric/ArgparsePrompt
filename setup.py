32fca0e7cb5d12639301f58a533641bfc724a77b
Michael Milton <ttmigueltt@gmail.com>
2017-05-25 17:02:14 +1000

    Convert readme to RST

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
 
 setup(
     name="argparse_prompt",
-    version="0.0.1",
-    packages=find_packages(),
+    long_description=long_description,
+    version="0.0.2",
+    py_modules=['argparse_prompt'],
     test_suite="test",
-    install_requires=[],
-    license="GPL"
+    license="GPL",
+    author="Michael Milton",
+    author_email="michael.r.milton@gmail.com",
+    description="Wrapper for the built-in Argparse, allowing missing command-line arguments to be filled in by the user via interactive prompts",
+    keywords="argparse prompt interactive argument",
+    url="https://github.com/MelbourneGenomics/ArgparsePrompt"
 )
